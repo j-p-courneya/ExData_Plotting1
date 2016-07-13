6392a258c03db6fc92a6573b122b1f0f97677a24
Jean-Paul Courneya <jcourn1@gmail.com>
2016-07-13 13:36:58 -0400

    initial commit in a nice shiny new repo with gitignore

---
+++ plot3.R
@@ -0,0 +1,5 @@
+#set working directory
+setwd("~/Desktop/Coursera/exploratory-data-analysis/ExData_Plotting1")
+#read in data
+household_power_consumption <- read.csv("~/Desktop/Coursera/exploratory-data-analysis/ExData_Plotting1/household_power_consumption.txt", sep=";", na.strings="?")
+
